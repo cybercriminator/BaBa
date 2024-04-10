433db5c68121e65ad00ea9e05f0fde0030ae04b8
CyBeR CRiMiNaToR <52202656+cybercriminator@users.noreply.github.com>
2024-04-10 14:17:19 +0400

    Add files via upload

---
+++ ax.php
@@ -0,0 +1,17 @@
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="title" content="Hacked By Ramil Feyziyev">
+<meta name="description" content="Ramil Feyziyev"
+<title>hacked by ramil feyziyev</title> 
+<body bgcolor="#000000">
+<body bgcolor="rgb(0,0,0)">
+<body bgcolor="black">
+<center>
+<br><br><br><br><br><br><br><br><br>
+<h3><font face="Verdana" color="#FFFFFF" style="font-size: 40pt"><font color="#c10000" size="8">></font>ramil</font></b><font face="Verdana" color="#ffffff" size="3"></h3>
+<font color="white" size="3"><?php echo $uname=php_uname();?>
+<br><br>
+<font color="red" size="3">[ <font color="white" size="3">az-xploit<font color="red"> ]</font> <font color="red" size="3">[ <font color="white" size="3">difai team<font color="red"> ]</font> <br> <font color="red" size="3">[ <font color="white" size="3">fedi bouzariata<font color="red"> ]</font> <font color="red" size="3">[ <font color="white" size="3">kenan54<font color="red"> ]</font>
+<!--eamilfeyziyev@gmail.com-->
+</center>
+</body>
+</html>
